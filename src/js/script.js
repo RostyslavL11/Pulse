21ff59591d5415958ef52d52f513d553ff59c6d8
Rostyslav <RostikL1994@gmail.com>
2023-09-27 14:59:22 +0300

    complete adaptation

--- src/js/script.js
+++ src/js/script.js
@@ -1,48 +1,17 @@
 $(document).ready(function () {
-    //     $('.carousel__inner').slick({
-    //         speed: 1200,
-    //         // adaptiveHeight: true,
-    //         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-    //         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-    //         responsive: [{
-    //             breakpoint: 992,
-    //             settings: {
-    //                 dots: true,
-    //                 arrows: false
-    //             }
-    //         }]
-    //     });
-
-
-    // const slider = tns({
-    //     container: '.carousel__inner',
-    //     items: 1,
-    //     slideBy: 'page',
-    //     autoplay: false,
-    //     controls: false,
-    //     nav: false,
-    //     responsive: {
-    //         640: {
-    //             edgePadding: 20,
-    //             gutter: 20,
-    //             items: 1
-    //         },
-    //         700: {
-    //             gutter: 30
-    //         },
-    //         900: {
-    //             items: 1
-    //         }
-    //     }
-    // });
-
-    // document.querySelector('.prev').addEventListener('click', function () {
-    //     slider.goTo('prev');
-    // });
-
-    // document.querySelector('.next').addEventListener('click', function () {
-    //     slider.goTo('next');
-    // });
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+        }]
+    });
 
 
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
@@ -82,4 +51,30 @@ $(document).ready(function () {
         })
     })
 
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: 'required',
+                phone: 'required',
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: "Пожалуйста, введите свое имя",
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask('+38 (999) 999-9999')
 });
