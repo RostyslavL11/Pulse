12a9f35219f7c64d769e0b4d86e1bd8492b2ed96
Rostyslav <RostikL1994@gmail.com>
2023-09-29 13:57:26 +0300

    adding animation

--- src/js/script.js
+++ src/js/script.js
@@ -98,4 +98,24 @@ $(document).ready(function () {
         })
         return false;
     });
+
+    // Smooth scroll and page up
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1000) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    })
+
+    $("a[hrief^='#']").click(function () {
+        const _href = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(_href).offset().top + "px"
+        });
+        return false;
+    });
+
+    new WOW().init();
+
 });
