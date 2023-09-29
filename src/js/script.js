0b7f8a6bc170bc043b3d513b8119baecc22f0349
Rostyslav <RostikL1994@gmail.com>
2023-09-29 15:27:49 +0300

    completed validation, fixing link in ul list

--- src/js/script.js
+++ src/js/script.js
@@ -26,7 +26,8 @@ $(document).ready(function () {
             $(this).on('click', function (e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+                // $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+                $('.catalog-item__info').eq(i).toggleClass('catalog-item__info_active');
             })
         });
     }
